b875d0dc18902212b3c81abb5b4fe835a0b1827f
jude <121809251+SJjude@users.noreply.github.com>
2025-11-27 14:29:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify 
 import pandas as pd
 import joblib
-import os
 
 app = Flask(__name__)
 
@@ -28,5 +27,4 @@ def predict():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use cloud platform port or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
